1a0b3d4c1b5a68aaf0b022b7d733ac69da84b63f
sai babu <saibabu@sais-MacBook-Air-3.local>
2024-03-06 21:35:10 +0530

    UPDATE.changed the height of the header section in mobile vers

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const header = document.querySelector('.header');
     const hamburger = document.querySelector('.HamburgerMenu');
     const navbar_r = document.querySelector('.navbar-right');
     const cross = document.querySelector('.hb-close');
@@ -7,11 +8,13 @@ document.addEventListener('DOMContentLoaded', function() {
         navbar_r.classList.toggle('show');
         cross.style.display = 'block';
         hamburger.style.display = "none";
+        header.style.height = '600px';
     });
 
     cross.addEventListener("click", () => {
         navbar_r.classList.toggle('show');
         cross.style.display = 'none';
         hamburger.style.display = "block";
+        header.style.height = '500px';
     });
 });
